8263267347a9a4acc0cf2a15b9778f9c48c350c6
KiLL3R_xRO <79046188+KiLL3R-xRO@users.noreply.github.com>
2021-07-06 22:40:32 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,19 @@
+import requests
 import os
-os.system('clear')
-print("""\33[33m█ █▄░█ █▀ █▀█ █▀▀ █▀▀ ▀█▀ █▀█ █▀█
-█ █░▀█ ▄█ █▀▀ ██▄ █▄▄ ░█░ █▄█ █▀▄""")
+os.system(  clear  )
+print("""\33[33m▀█▀ █▀▀▄ █▀▀ █▀▀█ █▀▀ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀█ 
+░█─ █──█ ▀▀█ █──█ █▀▀ █── ──█── █──█ █▄▄▀ 
+▄█▄ ▀──▀ ▀▀▀ █▀▀▀ ▀▀▀ ▀▀▀ ──▀── ▀▀▀▀ ▀─▀▀""")
 print("___________________________________________________")
 print("         \33[35m TOOL CREATED BY CEO xRO & Pappu")
 print("          CEO AND TERMUX ACADEMY")
+print("          Powered by THE F SOCIETY")
+print("          Special Thanks to Kavidu Sri(spy)")
 print("__________________________________________________")
-import requests
 link=input("Enter Site Link>>")
 print()
-print("[1]Inspect Website\n[2]Download Image Code\n[3]Check Status Code\n[4]Get site details\n[5]Check Json Data")
-choice=input('Enter your Choice>>')
+print("[1]Inspect Website\n[2]Download Image Code\n[3]Check Status Code\n[4]Get site details\n[5]Cookie Tracker\n[6]Check Available Requests\n[7]Full details about data\n[8]Check Encoding")
+choice=input(  Enter your Choice>>  )
 if choice=="1":
 	try:
 		r=requests.get(link)
@@ -20,7 +23,7 @@ if choice=="1":
 if choice=="2":
 		try:
 			r=requests.get(link)
-			with open('file.png','wb') as f:
+			with open( file.png , wb ) as f:
 				f.write(r.content)
 		except:
 			print("Sorry, unsupported url")
@@ -33,6 +36,30 @@ if choice=="4":
 				print(r.headers)
 			except:
 				print("Sorry, request denied")
+if choice== 5 :
+	try:
+		cookie_tracker={"User-Agent":"Mozilla/5.0 (Windows NT 6.1; U; nl; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 Opera 11.01"}
+		xro=requests.get(link,headers=cookie_tracker)
+		print(xro.cookies)
+	except:
+		print("Sorry,cannot receive information..")
+if  choice=="6":
+	try:
+		r=requests.get(link)
+		print(dir(r))
+	except:
+		print("Sorry,an error occured")
+if choice=="7":
+	try:
+		r=requests.get(link)
+		print(help(r))
+	except:
+		print("Sorry,no info received")
+if choice=="8":
+	try:
+		r=requests.get(link)
+		print(r.encoding)
+	except:
+		print("Permission Denied")
 print()
 print(".......Thanks For Using......")
-			
